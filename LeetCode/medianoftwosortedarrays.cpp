124dbd89b1e22df90ddd2e6134dd5e0bb8dfd058
Severin48 <severin2448@gmail.com>
2023-11-19 12:09:27 +0100

    Working binary search approach

--- LeetCode/medianoftwosortedarrays.cpp
+++ LeetCode/medianoftwosortedarrays.cpp
@@ -19,11 +19,37 @@ public:
     }
 
     double getMedianFromPartialVector(vector<int>& vec, int l) {
+        int m = (l / 2);
         if (l % 2 != 0) {
-            return vec[l / 2];
+            return vec[m];
         }
         else {
-            return ((double)vec[l / 2] + vec[(l / 2) - 1]) / 2.;
+            return ((double)vec[m] + vec[m - 1]) / 2.;
+        }
+    }
+
+    double getMedianFromPartialVector(vector<int>& vec, int l, int offset) {
+        int m = (l / 2) - offset;
+        if (l % 2 != 0) {
+            return vec[m];
+        }
+        else {
+            return ((double)vec[m] + vec[m - 1]) / 2.;
+        }
+    }
+
+    double getMedianFromPartialVector(vector<int>& vec, int l, int offset, int lof) {
+        int m = (l / 2) - offset;
+        if (l % 2 != 0) {
+            return vec[m];
+        }
+        else {
+            if (m - 1 < 0) {
+                return ((double)vec[m] + lof ) / 2.;
+            }
+            else {
+                return ((double)vec[m] + vec[m - 1]) / 2.;
+            }
         }
     }
 
@@ -136,10 +162,19 @@ public:
         int end1 = binarySearchFirstOccurence(nums1, e);
         int end2 = binarySearchFirstOccurence(nums2, e); // TODO: Brauche ich für end ein anderes binary search?
 
+        if (end1 == 0 && end2 == 0) {
+            if (l1 == 1) {
+                return nums1[0];
+            }
+            else {
+                return nums2[0];
+            }
+        }
+
         // TODO: Schauen, ob m in den Schnittbereich fällt, falls ja, mergen wie schon unten getan
         if (offset1 != offset2 && m < max(offset1, offset2)) {
             if (offset1 == 0) {
-                return getMedianFromPartialVector(nums1, l); 
+                return getMedianFromPartialVector(nums1, l);
             }
             else {
                 return getMedianFromPartialVector(nums2, l);
@@ -152,17 +187,17 @@ public:
             // TODO: INSERT in front or back ABER INSERT BEIM OFFSET BEACHTEN!!!!
             int offset = max(end1, end2);
             if (end1 > end2) {
-                return getMedianFromPartialVector(nums1, l); // TODO: Offset
+                return getMedianFromPartialVector(nums1, l, end2);
             }
             else {
-                return getMedianFromPartialVector(nums2, l); // TODO: Offset
+                return getMedianFromPartialVector(nums2, l, end1);
             }
         }
         
 
         int newLen = max(end1-offset1, end2-offset2);
         mv.reserve(newLen);
-
+        // Merging overlapping area into mv
         int v1, v2;
         for (int i = 0; i < newLen; i++) {
             v1 = nums1[offset1];
@@ -176,8 +211,24 @@ public:
                 offset2++;
             }
         }
-
-        return getMedianFromPartialVector(mv, l); // TODO: Offset
+        
+        // Case: Median requires one value from merged area and one left or right of it
+        int last_of_first_vec = 0;
+        bool lof_available = false;
+        if (offset1 > offset2) {
+            last_of_first_vec = nums1[offset1-1];
+        } else if (offset1 < offset2) {
+            last_of_first_vec = nums2[offset2-1];
+        }
+        //int first_of_last_vec = 0;
+        //if (end1 > end2) {
+        //    last_of_first_vec = nums1[offset1+1];
+        //}
+        //else if (end1 < end2) {
+        //    last_of_first_vec = nums2[offset2+1];
+        //}
+        
+        return getMedianFromPartialVector(mv, l, max(offset1, offset2), last_of_first_vec);
         // TODO: Median ist im gemergten Teil --> durch m einfach raussuchen mv[(m - max(offsets))]
         
         return median;
@@ -193,34 +244,34 @@ int main() {
     vector<int> v1;
     vector<int> v2;
     
-    //v1 = { 1, 3 };
-    //v2 = { 2 };
-    //m = s.findMedianSortedArrays(v1, v2);
-    //cout << m << endl;
+    v1 = { 1, 3 };
+    v2 = { 2 };
+    m = s.findMedianSortedArrays(v1, v2);
+    cout << m << endl;
 
-    //v1 = { };
-    //v2 = { 2,3 };
-    //m = s.findMedianSortedArrays(v1, v2);
-    //cout << m << endl;
+    v1 = { };
+    v2 = { 2,3 };
+    m = s.findMedianSortedArrays(v1, v2);
+    cout << m << endl;
 
-    //v1 = { };
-    //v2 = { 1 };
-    //m = s.findMedianSortedArrays(v1, v2);
-    //cout << m << endl;
+    v1 = { };
+    v2 = { 1 };
+    m = s.findMedianSortedArrays(v1, v2);
+    cout << m << endl;
 
-    //auto t1 = chrono::high_resolution_clock::now();
-    //for (int i = 0; i < n_runs; i++) {
-    //    v1 = { 1 };
-    //    v2 = { 2, 3, 4 };
-    //    m = s.findMedianSortedArrays(v1, v2);
-    //    //cout << m << endl;
-    //}
-    //auto t2 = high_resolution_clock::now();
-    //auto ms_int = duration_cast<milliseconds>(t2 - t1);
+    auto t1 = chrono::high_resolution_clock::now();
+    for (int i = 0; i < n_runs; i++) {
+        v1 = { 1 };
+        v2 = { 2, 3, 4 };
+        m = s.findMedianSortedArrays(v1, v2);
+        //cout << m << endl;
+    }
+    auto t2 = high_resolution_clock::now();
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
 
-    //duration<double, milli> ms_double = t2 - t1;
+    duration<double, milli> ms_double = t2 - t1;
 
-    //std::cout << ms_double.count() << "ms\n";
+    std::cout << ms_double.count() << "ms\n";
 
     v1 = { 1,2,3,4,5,6};
     v2 = { 1,2,3,7,11,12,13,16 };
